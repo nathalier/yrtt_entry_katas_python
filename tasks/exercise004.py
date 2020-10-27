3dcfb004dc92c50415658f685d96d9ba7df94419
Nathalie Rud <nathalier@gmail.com>
2020-10-27 14:07:36 +0000

    complete exercise004

--- tasks/exercise004.py
+++ tasks/exercise004.py
@@ -1,5 +1,7 @@
 # Move the first letter of each word to the end of it, then add "ay" 
 # to the end of the word. Leave punctuation marks untouched.
+import re
 
 def pig_it(text):
-    return
+    text = re.sub(r'\w+', lambda w: w.group()[1:] + w.group()[0] + 'ay', text)
+    return text
